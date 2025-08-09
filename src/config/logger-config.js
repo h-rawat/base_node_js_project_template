8f241c2c92d327195e07a647ba7b473893040cd9
Himanshu Rawat <himanshu.g.rawat@gmail.com>
2025-08-09 16:17:40 +0000

    change folder structure and setup logger

---
+++ src/config/logger-config.js
@@ -0,0 +1,19 @@
+const { createLogger, format, transports } = require('winston')
+const { combine, timestamp, label, printf } = format
+
+const customFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} : ${level} : ${message}`
+})
+
+const logger = createLogger({
+    format: combine(
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        customFormat
+    ),
+    transports: [
+        new transports.Console(), // shows logs on the console
+        new transports.File({ filename: 'combined.log' }) // logs the logs to a file
+    ]
+})
+
+module.exports = logger
